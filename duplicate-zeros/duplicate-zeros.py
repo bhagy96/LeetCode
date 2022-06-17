0bcd5983ea269339b10f1002208d530b40fe9aa3
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-06-17 11:03:36 -0700

    Time: 81 ms, Memory: 14.9 MB - LeetHub

--- duplicate-zeros/duplicate-zeros.py
+++ duplicate-zeros/duplicate-zeros.py
@@ -3,15 +3,10 @@ class Solution:
         """
         Do not return anything, modify arr in-place instead.
         """
-        # arr.insert(1,3)
-        # arr.pop()
-        # print(arr)
-        i=0
-        while i<(len(arr)):#for i in range(len(arr)):
-            # print(i)
+        i,L = 0,len(arr)
+        while i<L:
             if arr[i] == 0:
                 arr.insert(i+1,0)
                 arr.pop()
                 i+=1
             i+=1
-        # print(arr)
