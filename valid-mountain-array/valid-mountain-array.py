40d2fc8faa6727167f24baeeb83fafc194f06624
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-06-17 20:37:33 -0700

    Time: 402 ms, Memory: 15.4 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.py
+++ valid-mountain-array/valid-mountain-array.py
@@ -3,22 +3,17 @@ class Solution:
         l = len(arr)
         if l<3 or arr[0]>arr[1]:
             return False
-        flag,i,j = -1,0,1
+        flag,i,j = 0,0,1
         while j!=(l):
             if arr[i]==arr[j]:
                 return False
-            if flag==-1:
-                if arr[i]<arr[j]:
-                    flag = 0
-                else:
-                    return False
             if flag==0 and arr[i]>arr[j]:
                 flag = 1
             elif flag==1 and arr[i]<arr[j]:
                 return False
             i+=1
             j+=1
-        if flag==-1 or flag==0:
+        if flag==0:
             return False
         return True
             
