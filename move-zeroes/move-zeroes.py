c0d4f35833415b537a81240922ff64dd1f6063e5
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-06-19 09:27:32 -0700

    Time: 775 ms, Memory: 15.6 MB - LeetHub

--- move-zeroes/move-zeroes.py
+++ move-zeroes/move-zeroes.py
@@ -4,11 +4,19 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         c = nums.count(0)
-        L = len(nums)
+        # L = len(nums)
         for _ in range(c):
             nums.remove(0)
-            nums.insert(L-1,0)
+        nums += c*[0]
         return nums
+    
+    
+        # c = nums.count(0)
+        # L = len(nums)
+        # for _ in range(c):
+        #     nums.remove(0)
+        #     nums.insert(L-1,0)
+        # return nums
         # L = len(nums)
         # for i in range(L):
         #     if nums[i]==0:
