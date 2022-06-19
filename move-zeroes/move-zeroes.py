5027b4cb291928dcc483ccfd76dd7553125aa5fd
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-06-19 10:12:27 -0700

    Time: 188 ms, Memory: 15.5 MB - LeetHub

--- move-zeroes/move-zeroes.py
+++ move-zeroes/move-zeroes.py
@@ -4,15 +4,15 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         # c = nums.count(0)
-        c=0
-        for n in nums:
-            if n==0:
-                c+=1
-        # L = len(nums)
-        for _ in range(c):
-            nums.remove(0)
-        nums += c*[0]
-        return nums
+        # c=0
+        # for n in nums:
+        #     if n==0:
+        #         c+=1
+        # # L = len(nums)
+        # for _ in range(c):
+        #     nums.remove(0)
+        # nums += c*[0]
+        # return nums
     
     
         # c = nums.count(0)
@@ -21,6 +21,23 @@ class Solution:
         #     nums.remove(0)
         #     nums.insert(L-1,0)
         # return nums
+        
+        L = len(nums)
+        i=0
+        c=0
+        # for i in range(len(nums)):
+        while (i+c)<L:
+            # print("i",i)
+            if nums[i]==0:
+                nums.pop(i)                
+                c+=1
+            else:
+                i+=1
+            # print(nums)
+            # print("i+c",i+c,L)
+        nums += c*[0]
+        return nums
+    
         # L = len(nums)
         # for i in range(L):
         #     if nums[i]==0:
