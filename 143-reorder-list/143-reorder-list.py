c29a01afa6db8284d5c6e30ae607b7bead060ea9
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-06-24 19:40:06 -0700

    Time: 160 ms (24.16%), Space: 23.9 MB (63.88%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -9,80 +9,50 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
         
-        if not head.next:
-            return head
-        lst = []
-        fast = modi = head
-        while fast:
-            lst.append(fast.val)
-            fast = fast.next
-        fs=1
-        i, j, l = 1, len(lst)-1, 1
-        while i<=j:
-            if fs==0:
-                modi.next =  ListNode(lst[i])
-                modi = modi.next
-                fs = 1
-                i+=1
-            elif fs==1:
-                modi.next =  ListNode(lst[j])
-                modi = modi.next
-                fs = 0
-                j-=1
-        # return modi
-            
-            
-            
-#         lst = []
-#         fast = og = modi = head
-#         while fast:
-#             lst.append(fast.val)
-#             fast = fast.next
-#         fs=0
-#         j, l = len(lst)-1, 1
-#         while l!=len(lst):
-#             if fs==0:
-#                 og = modi.next
-#                 modi.next =  ListNode(lst[j])
-#                 modi = modi.next
-#                 fs = 1
-#                 j-=1
-#                 print(l,og.val)    
-#             elif fs==1 and og:
-#                 modi.next = ListNode(og.val)
-#                 modi = modi.next
-#                 fs = 0
-#             l+=1
         
+        slow = fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
         
-#         lst = []
-#         fast = modi = head
-#         while fast:
-#             lst.append(fast.val)
-#             fast = fast.next
-#         print(lst)
-#         # for i in range(len(lst)-1, -1, -1):
-#             # print(lst[i])
-#         L = len(lst)
-#         i,j  = 0, L-1
-#         l = 1
-#         while i<j:
-#             print(i,j,modi.val, lst[j])
-#             l-=1
-#             temp = modi.next
-#             modi.next = ListNode(lst[j])
-#             l+=3
-#             print(l)
-#             if l>=L:
-#                 break
-#             modi.next.next  = temp
+        print(slow.val, fast.val)
+        
+        prev, curr = None, slow.next
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        slow.next  = None
+        
+        head1, head2 = head, prev
+        while head2:
+            temp = head1.next
+            head1.next = head2
+            head1 = head2
+            head2 = temp
             
-#             modi = temp
-#             i+=1
-#             j-=1
-    
-#         # old_modi.next = None
-#         # old_modi.next = ListNode(modi.val)
-#         # print(old_modi.next.val)
-        # return head
+            
+            
+        # using a array
         
+        # if not head.next:
+        #     return head
+        # lst = []
+        # fast = modi = head
+        # while fast:
+        #     lst.append(fast.val)
+        #     fast = fast.next
+        # fs=1
+        # i, j, l = 1, len(lst)-1, 1
+        # while i<=j:
+        #     if fs==0:
+        #         modi.next =  ListNode(lst[i])
+        #         modi = modi.next
+        #         fs = 1
+        #         i+=1
+        #     elif fs==1:
+        #         modi.next =  ListNode(lst[j])
+        #         modi = modi.next
+        #         fs = 0
+        #         j-=1
