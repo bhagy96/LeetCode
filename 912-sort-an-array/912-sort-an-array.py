66661cf00a6513e506678b1e6d1865602d362f55
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-07-04 14:52:16 -0700

    Time: 1015 ms (42.51%), Space: 22.6 MB (26.91%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -1,3 +1,26 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        return sorted(nums)
+        
+        l = len(nums)
+        if l<=1:
+            return nums
+        mid = l//2
+        l,r  = self.sortArray(nums[:mid]), self.sortArray(nums[mid:])
+        return self.merge(l,r)
+    
+    def merge(self, l, r):
+        if not l or not r:
+            return l or r
+        ll = len(l) if len(l)<len(r) else len(r)
+        i,j, newarr = 0,0,[]
+        
+        while i<len(l) and j<len(r):
+            if l[i]<r[j]:
+                newarr.append(l[i])
+                i+=1
+            else:
+                newarr.append(r[j])
+                j+=1
+        return newarr + l[i:] + r[j:]
+        
+            
