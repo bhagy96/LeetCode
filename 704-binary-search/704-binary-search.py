a9f60d2be231d3a86c2efec96bba15c3865e1241
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-07-06 15:44:25 -0700

    Time: 342 ms (54.01%), Space: 15.4 MB (72.02%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,28 +1,44 @@
 class Solution:
     
-    def binsearch(self, start, end, arr, target):
+    def search(self, nums, target):
+        index = bisect.bisect_left(nums, target)
+        return index if index < len(nums) and nums[index] == target else -1
+    
+    # def search(self, nums, target):
+    #     l, r = 0, len(nums) - 1
+    #     while l <= r:
+    #         mid = (l + r) // 2
+    #         if target > nums[mid]:
+    #             l = mid + 1
+    #         elif target < nums[mid]:
+    #             r = mid - 1
+    #         else:
+    #             return mid
+    #     return -1
+    
+    
+#     def binsearch(self, start, end, arr, target):
         
         
-        mid = (start + end)//2
-        # print('start, end, mid ', start, end, mid)
-        if target==arr[mid]:
-            return mid
-        elif start>=end:
-            return -1
-        elif target<arr[mid]:
-            return self.binsearch(start, mid, arr, target)
-        elif target>arr[mid]:
-            return self.binsearch(mid+1, end, arr, target)
+#         mid = (start + end)//2
+#         if target==arr[mid]:
+#             return mid
+#         elif start>=end:
+#             return -1
+#         elif target<arr[mid]:
+#             return self.binsearch(start, mid, arr, target)
+#         elif target>arr[mid]:
+#             return self.binsearch(mid+1, end, arr, target)
             
     
-    def search(self, nums: List[int], target: int) -> int:
+#     def search(self, nums: List[int], target: int) -> int:
         
-        if len(nums)<=2:
-            # if target in nums:
-            try:
-                return nums.index(target)
-            except:
-                return -1
+#         if len(nums)<=2:
+#             # if target in nums:
+#             try:
+#                 return nums.index(target)
+#             except:
+#                 return -1
         
-        return self.binsearch(0, len(nums)-1, nums, target)
+#         return self.binsearch(0, len(nums)-1, nums, target)
             
