d7bf197f799733b6ff4e784f489dedd30aeb3046
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-07-06 23:47:43 -0700

    Time: 205 ms (74.20%), Space: 20.4 MB (40.90%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,31 +1,32 @@
 class Solution:
-    def binsearch(self, start, end, arr, target):
-        
-        
-        mid = (start + end)//2
-        if target==arr[mid]:
-            return mid
-        elif start>=end:
-            return -1
-        elif target<arr[mid]:
-            return self.binsearch(start, mid, arr, target)
-        elif target>arr[mid]:
-            return self.binsearch(mid+1, end, arr, target)
     
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
-        # row = -1
-        # for i in range(len(matrix)):
-        #     if target==matrix[i][-1]:
-        #         return True
-        #     elif target<matrix[i][-1]:
-        #         row = i
-        #         break
-        # if row==-1:
-        #     return False
-        for i in range(len(matrix)):
-            if target>= matrix[i][0] and target<=matrix[i][-1]:
-                # print('in')
-                if self.binsearch(0, len(matrix[i]), matrix[i], target) != -1:
-                    return True
+        i,j = len(matrix)-1, 0
+        while i>=0 and j<len(matrix[0]):
+            if target==matrix[i][j]:
+                return True
+            elif target<matrix[i][j]:
+                i-=1
+            elif target>matrix[i][j]:
+                j+=1
         return False
+                
+#     # O(mlogn)
+#     def binsearch(self, start, end, arr, target):
+#         mid = (start + end)//2
+#         if target==arr[mid]:
+#             return mid
+#         elif start>=end:
+#             return -1
+#         elif target<arr[mid]:
+#             return self.binsearch(start, mid, arr, target)
+#         elif target>arr[mid]:
+#             return self.binsearch(mid+1, end, arr, target)
+    
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         for i in range(len(matrix)):
+#             if target>= matrix[i][0] and target<=matrix[i][-1]:
+#                 if self.binsearch(0, len(matrix[i]), matrix[i], target) != -1:
+#                     return True
+#         return False
