ec3851083862db9c829b5f8288d954dec6d6f725
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-07-07 15:55:44 -0700

    Time: 55 ms (29.52%), Space: 13.9 MB (11.64%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.py
+++ 226-invert-binary-tree/226-invert-binary-tree.py
@@ -5,19 +5,39 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
-    def swap(self, node):
-        if node:
-            temp = node.left
-            node.left = node.right
-            node.right = temp
-        if node.left:
-            self.swap(node.left)
-        if node.right:
-            self.swap(node.right)
-        return node
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    #Iterative
+    def invertTree(self, root):
         if not root:
             return root
+
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack.extend([node.left, node.right])
+        return root
+        
+    
+    #shorter
+    
+    # def invertTree(self, root):
+    #     if root:
+    #         root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+    #         return root
+    
+#     def swap(self, node):
+#         if node:
+#             temp = node.left
+#             node.left = node.right
+#             node.right = temp
+#         if node.left:
+#             self.swap(node.left)
+#         if node.right:
+#             self.swap(node.right)
+#         return node
+#     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+#         if not root:
+#             return root
             
-        return self.swap(root)
+#         return self.swap(root)
