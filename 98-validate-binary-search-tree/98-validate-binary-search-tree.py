a2d20cc0ef64a704cd0553ed4109d81e5da3242d
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-07-08 17:09:00 -0700

    Time: 72 ms (44.01%), Space: 17.1 MB (13.94%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -5,15 +5,31 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+#     def isValidBST(self, root: TreeNode) -> bool:
+        
+#         def valid(node, left, right):
+#             if not node:
+#                 return True
+#             if node.val <= left or node.val >= right:
+#                 return False
+            
+#             return (valid(node.left, left, node.val) and
+#                     valid(node.right, node.val, right))
+        
+#         return valid(root, float("-inf"), float("inf"))
+    
+    def inorder(self,node, lst):
+        if not node: return
+        self.inorder(node.left, lst)
+        lst.append(node.val)
+        self.inorder(node.right, lst)
     def isValidBST(self, root: TreeNode) -> bool:
         
-        def valid(node, left, right):
-            if not node:
-                return True
-            if node.val >= right or node.val <= left:
+        lst = []
+        self.inorder(root,lst)
+        for i in range(1,len(lst)):
+            if lst[i-1]>=lst[i]:
                 return False
-            
-            return (valid(node.left, left, node.val) and
-                    valid(node.right, node.val, right))
+        return True
         
-        return valid(root, float("-inf"), float("inf"))
+        
