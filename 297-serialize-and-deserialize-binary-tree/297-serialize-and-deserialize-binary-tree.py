71430e18663b9ac40b7fac47ad418fe722f08e3e
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-07-09 18:10:11 -0700

    Time: 462 ms (10.99%), Space: 23.6 MB (10.07%) - LeetHub

---
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,56 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+    
+    def preorder(self,node,pre_str):
+        if node:
+            pre_str += ',' + str(node.val)
+            pre_str = self.preorder(node.left, pre_str)
+            pre_str = self.preorder(node.right, pre_str)
+        else:
+            pre_str += ',' + 'n'
+        return pre_str
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root: return ''
+        pre_str = ''
+        pre_str = self.preorder(root,pre_str)
+        return  pre_str[1:]
+    
+    def build(self,preorder):
+        if preorder:            
+            val = preorder.pop(0)
+            if val=='n':
+                return None
+            return TreeNode(int(val), self.build(preorder),self.build(preorder))
+            
+        
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        print(data)
+        if not data: return None
+        preorder = [str(x) for x in data.split(',')] 
+        print('pre',preorder)
+        return self.build(preorder)
+        
+        
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
