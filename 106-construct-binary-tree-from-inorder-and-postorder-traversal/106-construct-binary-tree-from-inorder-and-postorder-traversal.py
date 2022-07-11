895bbd3c0851e9e9251651f7cabc2c6d615ea697
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-07-11 14:11:22 -0700

    Time: 136 ms (62.61%), Space: 18.9 MB (73.01%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -5,11 +5,28 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        if inorder:
-            curr_val = postorder.pop()
-            ind = inorder.index(curr_val)
-            node = TreeNode(curr_val)
-            node.right = self.buildTree(inorder[ind+1:],postorder)
-            node.left =  self.buildTree(inorder[:ind],postorder)
-            return node
+    
+    # More Efficient
+    def buildTree(self, inorder, postorder):
+        map_inorder = {}
+        for i, val in enumerate(inorder): map_inorder[val] = i
+        print(map_inorder)
+        def recur(low, high):
+            if low > high: return None
+            x = TreeNode(postorder.pop())
+            # print('x', x)
+            mid = map_inorder[x.val]
+            print('m', mid)
+            x.right = recur(mid+1, high)
+            x.left = recur(low, mid-1)
+            return x
+        return recur(0, len(inorder)-1)
+    
+#     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+#         if inorder:
+#             curr_val = postorder.pop()
+#             ind = inorder.index(curr_val)   # Takes O(n) here
+#             node = TreeNode(curr_val)
+#             node.right = self.buildTree(inorder[ind+1:],postorder) # Extra stack calls
+#             node.left =  self.buildTree(inorder[:ind],postorder)    # Extra stack calls
+#             return node
