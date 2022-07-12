f619c778897b727a70a34d2f637016cd0f36e47c
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-07-11 21:11:22 -0700

    Time: 188 ms (84.83%), Space: 27.7 MB (81.70%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -1,29 +1,29 @@
 class Trie:
 
     def __init__(self, val = '', ):
-        self.val = val
-        self.node = []
         self.trie = {}
 
     def insert(self, word: str) -> None:
-        # for c in words:
         t = self.trie
         for c in word:
-            if c not in t: t[c] = {}
+            if c not in t:
+                t[c] = {}
             t = t[c]
-        t["-"] = True
+        t['-'] = True
 
     def search(self, word):
         t = self.trie
         for c in word:
-            if c not in t: return False
+            if c not in t:
+                return False
             t = t[c]
-        return "-" in t
+        return '-' in t
 
     def startsWith(self, prefix):
         t = self.trie
         for c in prefix:
-            if c not in t: return False
+            if c not in t:
+                return False
             t = t[c]
         return True
 
