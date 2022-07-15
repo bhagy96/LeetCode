de21d1a2ce917d899229c9db698ecb12953b4e35
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-07-15 16:08:05 -0700

    Time: 172 ms (25.31%), Space: 14.2 MB (28.75%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,18 +1,34 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        def dfs(i, cur_comb, total):
+    def combinationSum(self, candidates, target):
+        self.res = []
+        def dfs(ind, curr_sol, target):  
+            if target == 0:
+                self.res.append(curr_sol)
+
+            if target < 0 or ind >= len(candidates):
+                return
+
+            for i in range(ind, len(candidates)):
+                dfs(i, curr_sol + [candidates[i]] , target - candidates[i])
+        
+        
+        dfs(0, [], target)   
+        return self.res
             
-            if total==target:
-                res.append(cur_comb.copy())
-                return
-            if total>target or i>=len(candidates):
-                return
-            cur_comb.append(candidates[i])
-            dfs(i, cur_comb, total + candidates[i])
-            cur_comb.pop()
-            dfs(i+1, cur_comb, total)
-        dfs(0, [], 0)
-        return res
+#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+#         res = []
+#         def dfs(i, cur_comb, total):
+            
+#             if total==target:
+#                 res.append(cur_comb.copy())
+#                 return
+#             if total>target or i>=len(candidates):
+#                 return
+#             cur_comb.append(candidates[i])
+#             dfs(i, cur_comb, total + candidates[i])
+#             cur_comb.pop()
+#             dfs(i+1, cur_comb, total)
+#         dfs(0, [], 0)
+#         return res
 
             
