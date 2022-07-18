d2da2fe521925c6b88a4538785dc975babd43a31
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-07-18 12:38:10 -0700

    Time: 136 ms (28.71%), Space: 14 MB (65.83%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -4,28 +4,50 @@ class Solution:
         if image[sr][sc]==color: return image
         ogcolor = image[sr][sc]
         visited = set()
-        marked = [[False for j in range(cols)] for i in range(rows)]
-        # print(marked)
+        # marked = [[False for j in range(cols)] for i in range(rows)]
+        
         def bfs(r,c):
             q = collections.deque()
+            # visited.add((r,c))
             q.append((r,c))
             while q:
                 r,c = q.popleft()
-                # visited.add((r,c))
-                # print(r,c)
-                if not marked[r][c]:
+                if (r,c) not in visited:
+                # if not marked[r][c]:
+                    visited.add((r,c))
                     image[r][c] = color
-                    marked[r][c] = True
+                    # marked[r][c] = True
                     for dr,dc in [[1,0],[-1,0],[0,1],[0,-1]]:
                         nr,nc = r+dr, c+dc
-                        # print('infor',nr,nc)
-                        if nr in range(rows) and nc in range(cols):
-                            if not marked[nr][nc] and image[nr][nc]==ogcolor:
-                                # print('add', nr,nc)
+                        if nr in range(rows) and nc in range(cols) and (nr,nc) not in visited and image[nr][nc]==ogcolor:
                                 q.append((nr,nc))
             
             
         bfs(sr,sc)
         return image
+    
+#     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+#         rows, cols = len(image),len(image[0])
+#         if image[sr][sc]==color: return image
+#         ogcolor = image[sr][sc]
+#         visited = set()
+#         marked = [[False for j in range(cols)] for i in range(rows)]
+#         def bfs(r,c):
+#             q = collections.deque()
+#             q.append((r,c))
+#             while q:
+#                 r,c = q.popleft()
+#                 if not marked[r][c]:
+#                     image[r][c] = color
+#                     marked[r][c] = True
+#                     for dr,dc in [[1,0],[-1,0],[0,1],[0,-1]]:
+#                         nr,nc = r+dr, c+dc
+#                         if nr in range(rows) and nc in range(cols):
+#                             if not marked[nr][nc] and image[nr][nc]==ogcolor:
+#                                 q.append((nr,nc))
+            
+            
+#         bfs(sr,sc)
+#         return image
         
         
