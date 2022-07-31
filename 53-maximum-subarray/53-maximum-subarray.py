3df3ee4fe0b433a40a82c8daa426481b0cf82a8d
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-07-30 18:11:06 -0700

    Time: 1125 ms (49.77%), Space: 28.5 MB (9.96%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curmax, curmin = 0,0
+        res = max(nums)
+        for n in nums:
+            t = n+curmax
+            curmax = max(n+curmax, n+curmin, n)
+            curmin = min(t, n+curmin, n)
+            res = max(res, curmax)
+            # print(n, t, curmax, curmin, res)
+            
+        return res
