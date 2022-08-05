4bf77aa541f7e892423b204bc4e66fd918d78cc7
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-08-04 19:32:47 -0700

    Time: 78 ms (35.49%), Space: 14 MB (87.15%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,20 +1,60 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        
         m,n = len(s1), len(s2)
         if len(s3) != (m+n):
-            return False        
-        dp = [[False]* (n+1) for _ in range(m+1) ]
+            return False   
+        dp = [False for _ in range(n+1)]
         for i in range(m+1):
             for j in range(n+1):
-                
                 if i==0 and j==0:
-                    dp[i][j] = True
+                    dp[j] = True
                 elif i==0:
-                    dp[i][j] = dp[i][j-1] and (s2[j-1] == s3[i+j-1])
+                    dp[j] = dp[j-1] and (s2[j-1] == s3[i+j-1])
                 elif j==0:
-                    dp[i][j] = dp[i-1][j] and (s1[i-1] == s3[i+j-1])
+                    dp[j] = dp[j] and (s1[i-1] == s3[i+j-1])
                 else:
-                    dp[i][j] = (dp[i][j-1] and (s2[j-1] == s3[i+j-1])) or (dp[i-1][j] and (s1[i-1]==s3[i+j-1]))
+                    dp[j] = (dp[j-1] and (s2[j-1] == s3[i+j-1])) or (dp[j] and (s1[i-1]==s3[i+j-1]))
                 
-        return dp[m][n]
+        return dp[-1]
+#         m,n = len(s1), len(s2)
+#         if len(s3) != (m+n):
+#             return False        
+#         dp = [[False]* (n+1) for _ in range(m+1) ]
+#         for i in range(m+1):
+#             for j in range(n+1):
+                
+#                 if i==0 and j==0:
+#                     dp[i][j] = True
+#                 elif i==0:
+#                     dp[i][j] = dp[i][j-1] and (s2[j-1] == s3[i+j-1])
+#                 elif j==0:
+#                     dp[i][j] = dp[i-1][j] and (s1[i-1] == s3[i+j-1])
+#                 else:
+#                     dp[i][j] = (dp[i][j-1] and (s2[j-1] == s3[i+j-1])) or (dp[i-1][j] and (s1[i-1]==s3[i+j-1]))
+                
+#         return dp[-1][-1]
+    
+        
+                
+            
+            
+            
+    #     boolean dp[] = new boolean[s2.length() + 1];
+    #     for (int i = 0; i <= s1.length(); i++) {
+    #         for (int j = 0; j <= s2.length(); j++) {
+    #             if (i == 0 && j == 0) {
+    #                 dp[j] = true;
+    #             } else if (i == 0) {
+    #                 dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+    #             } else if (j == 0) {
+    #                 dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+    #             } else {
+    #                 dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+    #             }
+    #         }
+    #     }
+    #     return dp[s2.length()];
+    # }
+            
     
