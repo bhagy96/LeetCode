ddf8cb94f745858602fd8e3c3fc6797fe0b98a92
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-08-05 18:44:29 -0700

    Time: 876 ms (67.97%), Space: 19.1 MB (78.64%) - LeetHub

--- 134-gas-station/134-gas-station.py
+++ 134-gas-station/134-gas-station.py
@@ -1,24 +1,36 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         
-        # for i in range(len(gas)):
-        i = 0
-        while i<len(gas):
-            j, cnt, cumcost, flag = i, 0, 0, 0
-            while cnt<len(gas):
-                cumcost += gas[j]-cost[j]
-                if cumcost<0:
-                    flag = 1
-                    break
-                j+=1
-                if j==len(gas):
-                    j = 0
-                cnt+=1
-            if cnt==len(gas):
-                return i
-            if flag==1 and i>j:
-                return -1
-            i = j+1
-                
+        start, end = len(gas) - 1, 0
+        total = gas[start] - cost[start]
+
+        while start >= end:
+            while total < 0 and start >= end:
+                start -= 1
+                total += gas[start] - cost[start]
+            if start == end:
+                return start
+            total += gas[end] - cost[end]
+            end += 1
         return -1
+    
+#         i = 0
+#         while i<len(gas):
+#             j, cnt, cumcost, flag = i, 0, 0, 0
+#             while cnt<len(gas):
+#                 cumcost += gas[j]-cost[j]
+#                 if cumcost<0:
+#                     flag = 1
+#                     break
+#                 j+=1
+#                 if j==len(gas):
+#                     j = 0
+#                 cnt+=1
+#             if cnt==len(gas):
+#                 return i
+#             if flag==1 and i>j:
+#                 return -1
+#             i = j+1
+                
+#         return -1
     
