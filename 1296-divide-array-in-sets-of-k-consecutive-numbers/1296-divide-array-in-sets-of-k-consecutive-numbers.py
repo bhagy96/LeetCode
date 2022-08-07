c90606c5bd45f328d2bdb4bb969f140f087360c7
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-08-07 11:40:01 -0700

    Time: 844 ms (23.84%), Space: 31.4 MB (90.85%) - LeetHub

---
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPossibleDivide(self, hand: List[int], groupSize: int) -> bool:
+        l = len(hand)
+        if l % groupSize != 0:
+            return False
+        if groupSize==1: return True
+        dic = defaultdict(int)
+        for h in hand:
+            dic[h] += 1
+        heapify(hand)
+        
+        for i in range(l//groupSize):
+            start = heappop(hand)
+            while dic[start]==0:
+                start = heappop(hand)
+            for i in range(groupSize):
+                dic[start] -= 1
+                if dic[start] < 0:
+                    return False
+                start += 1
+        return True
