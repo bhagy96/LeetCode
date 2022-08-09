6e08796c5ef0cfaf85533cf13bfbca1adf98728b
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-08-08 19:47:09 -0700

    Time: 71 ms (7.93%), Space: 13.9 MB (58.70%) - LeetHub

---
+++ 66-plus-one/66-plus-one.py
@@ -0,0 +1,17 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        f, s = 0,1
+        for i in range(len(digits)-1, -1,-1):
+            s += digits[i] 
+            if s>=10:
+                digits[i] = s%10
+                s = s//10
+                if i==0:
+                    f = 1
+            else:
+                digits[i] = s
+                return digits
+        if f==1:
+            digits.insert(0,1)
+        return digits
+                    
