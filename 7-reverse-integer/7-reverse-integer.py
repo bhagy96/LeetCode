401cf7777a1664ee82070f41a1fa13b088bd0dc6
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-08-13 16:34:34 -0700

    Time: 37 ms (87.59%), Space: 13.9 MB (16.04%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -4,9 +4,7 @@ class Solution:
         if x<0: 
             f = 1
             x *= -1
-        s = format(int(str(x)[::-1]), 'b')
-        # if len(s)>32: return 0
-        re = int(s, 2)
+        re = int(format(int(str(x)[::-1]), 'b'), 2)
         if re>((2**31)-1): return 0
         if f:
             return re*-1
