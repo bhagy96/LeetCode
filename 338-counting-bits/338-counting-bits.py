83d49194bbe45204a12752d4ff590c2401864e26
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-08-13 11:09:05 -0700

    Time: 138 ms (56.23%), Space: 20.9 MB (29.70%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,19 +1,33 @@
 class Solution:
-    import math
- 
-    def highestPowerof2(self,n):
-        return int(math.log(n, 2));
+    
     def countBits(self, n: int) -> List[int]:
-        if n==0: return [0]
-        start, end, e = 0,0,0
-        dp = [0]
-        power = self.highestPowerof2(n) + 1
-        for i in range(power):
-            for j in range(int(pow(2, i))):
-                if len(dp)>n:
-                    return dp
-                dp.append(dp[j]+1)
+        dp = [0] * (n + 1)
+        offset = 1
+
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
         return dp
+
+    
+    # dp method 
+    
+#     import math
+ 
+#     def highestPowerof2(self,n):
+#         return int(math.log(n, 2));
+#     def countBits(self, n: int) -> List[int]:
+#         if n==0: return [0]
+#         start, end, e = 0,0,0
+#         dp = [0]
+#         power = self.highestPowerof2(n) + 1
+#         for i in range(power):
+#             for j in range(int(pow(2, i))):
+#                 if len(dp)>n:
+#                     return dp
+#                 dp.append(dp[j]+1)
+#         return dp
             
         
             
