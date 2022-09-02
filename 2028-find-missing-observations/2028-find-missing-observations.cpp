aebb2d8239efa1f28e9736ceda526b73c1ab846e
Bhagyesh Gaikwad <35718568+bhagy96@users.noreply.github.com>
2022-09-01 20:12:30 -0700

    Time: 201 ms (83.13%), Space: 113.7 MB (79.88%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.cpp
+++ 2028-find-missing-observations/2028-find-missing-observations.cpp
@@ -1,14 +1,16 @@
 class Solution {
-public:
-    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
-        int curSum = accumulate(rolls.begin(), rolls.end(), 0), m = rolls.size();
-        int missingSum = mean * (n + m) - curSum;
-        if (missingSum < n || missingSum > 6*n) return {};
-        
-        int part = missingSum / n, rem = missingSum % n;
-        vector<int> ans(n, part);
-        for (int i = 0; i < rem; ++i) 
-            ++ans[i];
-        return ans;
-    }
+ public:
+  vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+    const int targetSum = (rolls.size() + n) * mean;
+    int missingSum = targetSum - accumulate(begin(rolls), end(rolls), 0);
+    if (missingSum > n * 6 || missingSum < n)
+      return {};
+
+    vector<int> ans(n, missingSum / n);
+    missingSum %= n;
+    for (int i = 0; i < missingSum; ++i)
+      ++ans[i];
+
+    return ans;
+  }
 };
